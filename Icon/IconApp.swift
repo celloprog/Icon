3af069f23d9456c9b68d1cfb67fdd1c66624718f
Maxim Frolov <celloprog@gmail.com>
2024-12-12 07:40:12 -0500

    Initial Commit

---
+++ Icon/IconApp.swift
@@ -0,0 +1,17 @@
+//
+//  IconApp.swift
+//  Icon
+//
+//  Created by Maxim Frolov on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct IconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
